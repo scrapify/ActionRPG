6f08b6c35e499a1a9418cce53fab531da1c2a5d2
Vijayakumar <vijayakumar83@outlook.in>
2020-05-12 23:57:12 +0530

    Initial commit

---
+++ ElderWand/Assets/Scripts/Mover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetObject;
+
+    //defineing a Ray
+    Ray lastRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the position of the sphere and assign to NavMesh Destination
+        //GetComponent<NavMeshAgent>().destination = targetObject.transform.position;
+
+        //Get Mousedown
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Use Camera screen point to ray to case the ray
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveCursor();
+
+        }
+        //use drawray to display the ray
+        // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+
+
+
+
+
+
+    }
+
+    public void MoveCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(ray, out hit);
+        if (isHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
