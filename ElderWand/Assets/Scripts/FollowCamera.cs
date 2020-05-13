b2d7d3cc454aaa234e8cad74abd00d4dea58dc76
Vijayakumar <vijayakumar83@outlook.in>
2020-05-14 00:06:33 +0530

    Added camera movement to character started using animator, animation and Blend tree

---
+++ ElderWand/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    public Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Target.position;
+    }
+}
